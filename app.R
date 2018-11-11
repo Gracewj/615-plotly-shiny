ee5d39dc671c6530a0afdffd1e4d5641433913f6
Qianhui Rong <qhrong@bu.edu>
2018-11-11 16:06:47 -0500

    .

--- app.R
+++ app.R
@@ -3,8 +3,8 @@ library(shiny)
 library(magrittr)
 library(dplyr)
 
-#In data cleaning process, we first delete the contributors from the othercountries(outside the USA)
-#Then group the data by state
+#Divide into four parts: total/rep/dom/ind
+#Total donate amount data
 data <- read.csv("11-5 MASSCONTRIBUTIONS-csv.csv")
 state<- read.csv("states.csv")
 data$date <- as.Date(data$date,"%Y-%m-%d")
@@ -15,6 +15,23 @@ total_donate <- data %>%
 colnames(total_donate)[1]<-"st_abrev"
 total_donate %<>% inner_join(y = state,by = "st_abrev")
 
+#Republican donate amount data
+data %>% filter(party=="R") %>% group_by(state) %>% 
+  summarise(sum_donate=sum(amount)) -> rep_amount
+colnames(rep_amount)[1]<-"st_abrev"
+rep_amount %<>% inner_join(y = state,by = "st_abrev")
+
+#Democrate donate amount data
+data %>% filter(party=="D") %>% group_by(state) %>% 
+  summarise(sum_donate=sum(amount)) -> dem_amount
+colnames(dem_amount)[1]<-"st_abrev"
+dem_amount %<>% inner_join(y = state,by = "st_abrev")
+
+#Independent donate amount data 
+data %>% filter(party=="I") %>% group_by(state) %>% 
+  summarise(sum_donate=sum(amount)) -> ind_amount
+colnames(ind_amount)[1]<-"st_abrev"
+ind_amount %<>% inner_join(y = state,by = "st_abrev") 
 
 ui <- fluidPage(
   plotlyOutput("plot"),
@@ -24,15 +41,19 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   
   output$plot <- renderPlotly({
+    # give state boundaries a white border
+    l <- list(color = toRGB("white"), width = 2)
     # specify some map projection/options
     g <- list(
       scope = 'usa',
       projection = list(type = 'albers usa'),
+      showlakes = TRUE,
       lakecolor = toRGB('white')
     )
     plot_ly(z = total_donate$sum_donate, text = total_donate$st_name, 
             locations = total_donate$st_abrev,
             type = 'choropleth', locationmode = 'USA-states') %>%
+      colorbar(title = "USD") %>% 
       layout(geo = g)
   })
   
