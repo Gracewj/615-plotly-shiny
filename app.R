666380ab0d632c3b1d01ce32491c56fe65958c9f
Longhao Chen <lc18@bu.edu>
2018-11-15 16:43:21 -0500

    ..

--- app.R
+++ app.R
@@ -21,16 +21,14 @@ data %>% filter(party=="R") %>% group_by(state) %>%
   summarise(sum_donate=sum(amount)) -> rep_amount
 colnames(rep_amount)[1]<-"st_abrev"
 rep_amount %<>% inner_join(y = state,by = "st_abrev")
-colnames(rep_amount)[2] <- "rep_sum"
-rep_amount %<>% mutate(party=rep("Repu",dim(rep_amount)[1]))
+rep_amount %<>% mutate(party=rep("Republican",dim(rep_amount)[1]))
 
 #Democrate donate amount data
 data %>% filter(party=="D") %>% group_by(state) %>% 
   summarise(sum_donate=sum(amount)) -> dem_amount
 colnames(dem_amount)[1]<-"st_abrev"
 dem_amount %<>% inner_join(y = state,by = "st_abrev")
-colnames(dem_amount)[2] <- "dem_sum"
-dem_amount %<>% mutate(party=rep("Demo",dim(dem_amount)[1]))
+dem_amount %<>% mutate(party=rep("Democrats",dim(dem_amount)[1]))
 
 #Republican & Democrate together
 both_amount <- inner_join(dem_amount,rep_amount, by=c("st_abrev","st_name"))
@@ -40,12 +38,18 @@ both_amount <- inner_join(dem_amount,rep_amount, by=c("st_abrev","st_name"))
 data %>% filter(party=="I") %>% group_by(state) %>% 
   summarise(sum_donate=sum(amount)) -> ind_amount
 colnames(ind_amount)[1]<-"st_abrev"
-ind_amount %<>% inner_join(y = state,by = "st_abrev")  
-ind_amount %<>% mutate(party=rep("Inde",dim(ind_amount)[1]))
+ind_amount %<>% full_join(y = state,by = "st_abrev")  
+ind_amount %<>% mutate(party=rep("Independent",dim(ind_amount)[1]))
+ind_amount$st_name.x <- NULL
+colnames(ind_amount)[4] = "st_name"
+ind_amount$sum_donate[is.na(ind_amount$sum_donate)] = 0
+colnames(ind_amount)[4] <- "party"
+ind_amount <- ind_amount[c("st_abrev", "sum_donate", "st_name", "party")]
+ind_amount<-ind_amount[-9,]
 
 # Total data including party
 total_party<-rbind(dem_amount,ind_amount,rep_amount)
-
+total_party$sum_donate <- log(total_party$sum_donate+1)
 #Min of data and Max of data
 minDate <- as.Date("2016-06-05","%Y-%m-%d")
 maxDate <- as.Date("2018-10-17","%Y-%m-%d")
@@ -59,12 +63,9 @@ ui <- fluidPage(
   # 
   # sidebarLayout(      
   # 
-  sliderInput("Date range slider", "Date Range",
-              min = as.Date(as.yearmon(minDate)),
-              max = as.Date(as.yearmon(maxDate)),
-              value = c(as.Date(as.yearmon(minDate)),as.Date(as.yearmon("2016-07-05")))),
+ 
   
-  selectInput(inputId = "party",label = "Data",choices=c("Total","Democrates","Republicans","Independents"),
+  selectInput(inputId = "party",label = "Data",choices=c("Democrats","Republican","Independent"),
                helpText("Choose the data you want to see in the map"),multiple = FALSE)
   
   # mainPanel(
@@ -91,11 +92,11 @@ server <- function(input, output, session) {
     
   
       
-    plot_ly(z = total_donate$sum_donate, text = total_donate$st_name, 
+    plot_ly(z = total_party$sum_donate[total_party$party == input$party], text = total_donate$st_name, 
             locations = total_donate$st_abrev,
             type = 'choropleth', locationmode = 'USA-states',
             colorscale='PuBu') %>%
-      colorbar(title = "USD") %>% 
+      colorbar(title = "USD in log") %>% 
       layout(geo = g)
   })
 
